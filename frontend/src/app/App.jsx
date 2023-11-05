11831b5f77f69ce6818d6eda3b56e9f341625b39
baeseongjae <3574214@naver.com>
2023-11-06 03:25:15 +0900

    ✨ [Feat] 리액트 라우터 사용하여 페이지 이동 구현

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,13 +1,22 @@
-import Join from '../../src/pages/Join/Join'
-// import reactLogo from '../assets/react.svg'
-// import viteLogo from '/vite.svg'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import { BaseLayout } from '../../src/components/Layout/Layout';
+import LandingPage from '../../src/pages/LandingPage/LandingPage';
+import Join from '../../src/pages/Join/Join';
+import Login from '../../src/pages/Login/Login';
 
 function App() {
   return (
-    <>
-      <Join />
-    </>
+    <Router>
+      <BaseLayout>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/join" element={<Join />}/>
+          <Route path="/login" element={<Login />}/>
+        </Routes>
+      </BaseLayout>
+    </Router>
   )
 }
 
