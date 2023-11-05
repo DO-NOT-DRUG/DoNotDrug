18fe542829a586c94e0e0d0eae79ec79b4001251
baeseongjae <3574214@naver.com>
2023-11-05 21:09:58 +0900

    ✨ [Feat] 폼 제출 함수 handleSubmitForm 기능 구현

--- frontend/src/pages/Join/Join.jsx
+++ frontend/src/pages/Join/Join.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-
+import axios from 'axios';
 import { Button, Container, Input } from '../../components';
+
 
 const Form = styled.form`
   width: 640px;
@@ -33,10 +34,69 @@ const RadioContainer = styled.div`
   gap: 1rem;
 `;
 
+
+
+
 function Join() {
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [rePassword, setRePassword] = useState('');
+
+  
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  }
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  }
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  }
+
+  const handleRePasswordChange = (event) => {
+    setRePassword(event.target.value);
+  }
+
+  // 회원가입 폼 제출 요청 
+  const handleSubmitForm = async (event) => {
+    event.preventDefault();
+
+    if(password !== rePassword) {
+      alert('비밀번호가 일치하지 않습니다.');
+      return;
+    }
+
+    const joinData = {
+      email,
+      name,
+      password,
+    };
+
+    try {
+      const response = await axios.post('http://localhost:8080/join', joinData);
+      alert('회원가입 성공: ' + response.data.message);
+      /* 로그인 페이지 이동 로직 */
+    } catch (error) {
+      if (error.response) {
+        // 요청이 이루어졌으나 서버가 2xx 범위가 아닌 상태 코드로 응답
+        alert('회원가입 실패: ' + error.response.data.message);
+      } else if (error.request) {
+        // 요청이 이루어 졌으나 응답을 받지 못함
+        alert('회원가입 요청 실패: 서버에서 응답이 없습니다.');
+      } else {
+        // 요청을 설정하는 중에 문제가 발생함
+        alert('회원가입 요청 중 오류 발생: ' + error.message);
+      }
+    }
+  }
+
+
   return (
     <Container as="section" align="center" width="widthMedium">
-      <Form>
+      <Form onSubmit={handleSubmitForm}>
         <Ul>
           <Li>
             <Label htmlFor="name">이름</Label>
@@ -46,6 +106,7 @@ function Join() {
               name="name"
               placeholder='이름을 입력해주세요' 
               required 
+              onChange={handleNameChange}
             />
           </Li>
           <Li>
@@ -56,8 +117,9 @@ function Join() {
               name="email"
               placeholder='이메일을 입력해주세요' 
               required
+              onChange={handleEmailChange}
             />
-            <Button isSecondary >중복확인</Button>
+            <Button isSecondary>이메일 인증</Button>
           </Li>
           <Li>
             <Label htmlFor="loginPassword">비밀번호</Label>
@@ -67,6 +129,7 @@ function Join() {
               name="loginPassword"
               placeholder='비밀번호를 입력해주세요'
               required
+              onChange={handlePasswordChange}
             />
           </Li>
           <Li>
@@ -77,10 +140,11 @@ function Join() {
               name="confirmPassword"
               placeholder='비밀번호를 확인해주세요'
               required
+              onChange={handleRePasswordChange}
             />
           </Li>
         </Ul>
-        <Button>회원가입하기</Button>
+        <Button type="submit">회원가입하기</Button>
       </Form>
     </Container>
   )
