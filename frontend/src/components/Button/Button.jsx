2ffb9974e67f373d0cb8e6867f3f493502e55f7e
baeseongjae <3574214@naver.com>
2023-11-05 21:07:54 +0900

    🐛 [Fix] 'isSecondary' prop 필터링

--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import PropTypes from 'prop-types';
 
-const StyledButton = styled.button`
+const StyledButton = styled(({ isSecondary, ...rest }) => <button {...rest} />)`
   padding: 16px 0;
   cursor: pointer;
   border: none;
@@ -11,9 +11,10 @@ const StyledButton = styled.button`
 
   background-color: ${props => props.isSecondary ? 'white' : '#6B4EFF'};
   color: ${props => props.isSecondary ? '#6B4EFF' : 'white'};
-  border: ${props => props.isSecondary && `1px solid #6B4EFF`};
-  margin-left: ${props => props.isSecondary && '8px'};
+  border: ${props => props.isSecondary ? '1px solid #6B4EFF' : 'none'};
+  margin-left: ${props => props.isSecondary ? '8px' : '0'};
   width: ${props => props.isSecondary ? '20%' : '50%'};
+  
   &:hover {
     background-color: #5538EE;
     color: white;
