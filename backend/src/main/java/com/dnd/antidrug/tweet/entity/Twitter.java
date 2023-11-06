8bc4a975b2cd7270757ee7a108bedc292ee45825
조태규 <hey03312@gmail.com>
2023-11-07 00:29:58 +0900

    [Feat] Twitter 도메인 추가

---
+++ backend/src/main/java/com/dnd/antidrug/tweet/entity/Twitter.java
@@ -0,0 +1,38 @@
+package com.dnd.antidrug.tweet.entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Twitter {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    private String user;
+
+    private String nickname;
+
+    @Column(columnDefinition="TEXT")
+    private String tweet;
+
+    private String url;
+
+    private String link;
+
+}
