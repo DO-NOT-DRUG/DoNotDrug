07bb465904f4bd8a572e3bc3ed36ffd82e54c2b1
zero0205 <etety39@naver.com>
2023-11-07 02:29:25 +0900

    ✨ Feat: 마약사범이 게시글 검색

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -5,6 +5,7 @@ import { BaseLayout } from '@/components/Layout/Layout';
 import LandingPage from '@/pages/LandingPage/LandingPage';
 import Join from '@/pages/Join/Join';
 import Login from '@/pages/Login/Login';
+import Search from '@/pages/Search/Search';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/join" element={<Join />}/>
           <Route path="/login" element={<Login />}/>
+          <Route path='/search' element={<Search/>}/>
         </Routes>
       </BaseLayout>
     </Router>
