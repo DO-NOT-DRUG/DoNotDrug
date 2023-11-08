1f388aa64538ea973e9de5fec2955ad34140e9ef
baeseongjae <3574214@naver.com>
2023-11-09 00:25:06 +0900

    ✨ [Feat] ButtonWrapper 컴포넌트 분리

---
+++ frontend/src/components/ButtonWrapper/ButtonWrapper.jsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const StyledButtonWrapper = styled.div`
+  width: 100%;
+  padding: 50px 0 0;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  color: #6B4EFF;
+`;
+
+export function ButtonWrapper({ children, ...restProps}) {
+
+  return (
+    <StyledButtonWrapper {...restProps}>
+      {children}
+    </StyledButtonWrapper>
+  )
+}
--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,6 +1,7 @@
 /* ------------------------------- 공통 ------------------------------ */
 export * from './Container/Container';
 export * from './Button/Button';
-export * from './Input/Input';
+export * from './Input/General/Input';
 export * from './Form/Form';
 export * from './Title/Title';
+export * from './ButtonWrapper/ButtonWrapper';
