22b7d67e18669ae1e7297479e743bf994815c6d6
이규민 <lkm454545@naver.com>
2023-11-09 23:57:28 +0900

    fix: 로그인 API 수정

--- backend/src/main/java/com/dnd/antidrug/member/dto/request/LoginRequest.java
+++ backend/src/main/java/com/dnd/antidrug/member/dto/request/LoginRequest.java
@@ -6,7 +6,7 @@ import javax.validation.constraints.NotBlank;
 public record LoginRequest(
     @NotBlank String email,
     @NotBlank String loginPassword,
-    @NotBlank Role role
+    Role role
     ) {
 
 }
